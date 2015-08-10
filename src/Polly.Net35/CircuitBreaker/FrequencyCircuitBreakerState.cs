2de319a08ac56f391cdb9815a6a8ea20f424efee
reisenberger <dmr@ebs.hw.ac.uk>
2015-08-10 11:21:29 +0100

    Another possible sketch for a frequency-based CircuitBreaker
    Another possible sketch for a frequency-based CircuitBreaker.  Breaks
    the
    circuit on exceeding a ratio of N failures in T calls.

---
+++ src/Polly.Net35/CircuitBreaker/FrequencyCircuitBreakerState.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Polly.Utilities;
+
+namespace Polly.CircuitBreaker
+{
+    internal class FrequencyCircuitBreakerState : ICircuitBreakerState
+    {
+        private readonly TimeSpan _durationOfBreak;
+        private readonly int _exceptionsAllowedBeforeBreaking;
+        private readonly int _inTotalCallCount;
+
+        private int _count;
+        private readonly bool[] _failures;
+        private int _writeLocation;
+
+        private DateTime _blockedTill;
+        private Exception _lastException;
+
+        private readonly object _lock = new object();
+
+        public FrequencyCircuitBreakerState(int exceptionsAllowedBeforeBreaking, int inTotalCallCount, TimeSpan durationOfBreak)
+        {
+            _durationOfBreak = durationOfBreak;
+            _exceptionsAllowedBeforeBreaking = exceptionsAllowedBeforeBreaking;
+            _inTotalCallCount = inTotalCallCount;
+
+            _failures = new bool[inTotalCallCount];
+            _writeLocation = 0;
+            ResetMetric();
+
+            _blockedTill = DateTime.MinValue;
+        }
+
+        public Exception LastException
+        {
+            get
+            {
+                using (TimedLock.Lock(_lock))
+                {
+                    return _lastException;
+                }
+            }
+        }
+
+        public bool IsBroken
+        {
+            get
+            {
+                using (TimedLock.Lock(_lock))
+                {
+                    return SystemClock.UtcNow() < _blockedTill;
+                }
+            }
+        }
+
+        public void Reset()
+        {
+            using (TimedLock.Lock(_lock))
+            {
+                _blockedTill = DateTime.MinValue;
+                RecordOutcome(true);
+            }
+        }
+
+        private void ResetMetric()
+        {
+            for (int i = 0; i < _inTotalCallCount; i++)
+            {
+                _failures[i] = false;
+            }
+        }
+
+        private void RecordOutcome(bool success)
+        {
+            _failures[_writeLocation] = !success;
+            _writeLocation = (_writeLocation + 1) % _inTotalCallCount;
+
+        }
+
+        public void TryBreak(Exception ex)
+        {
+            using (TimedLock.Lock(_lock))
+            {
+                _lastException = ex;
+
+                if (_count < _exceptionsAllowedBeforeBreaking) { _count += 1; }
+
+                RecordOutcome(false);
+
+                if (_count >= _exceptionsAllowedBeforeBreaking)
+                {
+                    if (_failures.Count(s => s) > _exceptionsAllowedBeforeBreaking) // Could use a for loop to count rather than .Count(), given simplicity of _failures structure
+                    {
+                        BreakTheCircuit();
+                    }
+                }
+            }
+        }
+
+        void BreakTheCircuit()
+        {
+            var willDurationTakeUsPastDateTimeMaxValue = _durationOfBreak > DateTime.MaxValue - SystemClock.UtcNow();
+            _blockedTill = willDurationTakeUsPastDateTimeMaxValue ?
+                               DateTime.MaxValue :
+                               SystemClock.UtcNow() + _durationOfBreak;
+
+            ResetMetric();
+        }
+    }
+}
--- src/Polly.Net45/CircuitBreakerSyntaxAsync.cs
+++ src/Polly.Net45/CircuitBreakerSyntaxAsync.cs
@@ -14,7 +14,7 @@ namespace Polly
         /// exceptions that are handled by this policy are raised. The circuit will stay
         /// broken for the <paramref name="durationOfBreak"/>. Any attempt to execute this policy
         /// while the circuit is broken, will immediately throw a <see cref="BrokenCircuitException"/> containing the exception 
-        /// that broke the cicuit.
+        /// that broke the circuit.
         /// </para>
         /// <para>If the first action after the break duration period results in an exception, the circuit will break
         /// again for another <paramref name="durationOfBreak"/>, otherwise it will reset.
@@ -31,6 +31,35 @@ namespace Polly
             if (exceptionsAllowedBeforeBreaking <= 0) throw new ArgumentOutOfRangeException("exceptionsAllowedBeforeBreaking", "Value must be greater than zero.");
 
             var policyState = new CircuitBreakerState(exceptionsAllowedBeforeBreaking, durationOfBreak);
+            return new Policy(action => CircuitBreakerPolicy.ImplementationAsync(action, policyBuilder.ExceptionPredicates, policyState));
+
+        }
+
+        /// <summary>
+        /// <para> Builds a <see cref="Policy" /> that will function like a Circuit Breaker.</para>
+        /// <para>The circuit will break if exceptions that are handled by this policy are raised such that
+        /// <paramref name="exceptionsAllowedBeforeBreaking" /> or more occur in any run of <paramref name="inTotalCallCount" /> calls.  For example, you might define that your failure threshold is 3/10, to break on 3 exceptions in any 10 calls.  The circuit will stay
+        /// broken for the <paramref name="durationOfBreak" />. Any attempt to execute this policy
+        /// while the circuit is broken, will immediately throw a <see cref="BrokenCircuitException" />
+        /// containing the exception that broke the circuit.
+        /// </para>
+        /// <para>After the break duration, the circuit will open again.  Subsequent calls will break the circuit according to the original criteria: if the proportion of handled failures exceeds <paramref name="exceptionsAllowedBeforeBreaking" /> per <paramref name="inTotalCallCount" /> calls.
+        /// </para>
+        /// </summary>
+        /// <param name="policyBuilder">The policy builder.</param>
+        /// <param name="exceptionsAllowedBeforeBreaking">The number of exceptions that are allowed before opening the circuit.</param>
+        /// <param name="inTotalCallCount">A total call count which the number of exceptions should be considered a proportion of.</param>
+        /// <param name="durationOfBreak">The duration the circuit will stay open before resetting.</param>
+        /// <returns>The policy instance.</returns>
+        /// <exception cref="System.ArgumentOutOfRangeException">exceptionsAllowedBeforeBreaking;Value must be greater than zero.</exception>
+        /// <exception cref="System.ArgumentOutOfRangeException">inTotalCallCount;Value must be greater than exceptionsAllowedBeforeBreaking.</exception>
+        /// <remarks>(see "Release It!" by Michael T. Nygard fi)</remarks>
+        public static Policy CircuitBreaker(this PolicyBuilder policyBuilder, int exceptionsAllowedBeforeBreaking, int inTotalCallCount, TimeSpan durationOfBreak)
+        {
+            if (exceptionsAllowedBeforeBreaking <= 0) throw new ArgumentOutOfRangeException("exceptionsAllowedBeforeBreaking", "Value must be greater than zero.");
+            if (!(exceptionsAllowedBeforeBreaking < inTotalCallCount)) throw new ArgumentOutOfRangeException("inTotalCallCount", "Value must be greater than exceptionsAllowedBeforeBreaking.");
+
+            var policyState = new FrequencyCircuitBreakerState(exceptionsAllowedBeforeBreaking, inTotalCallCount, durationOfBreak);
             return new Policy(action => CircuitBreakerPolicy.ImplementationAsync(action, policyBuilder.ExceptionPredicates, policyState));
         }
     }
